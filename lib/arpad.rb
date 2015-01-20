54469a3bfd322417f573cae9e7189b2f4ddbb4f7
Elijah Kim <kimelijah90@gmail.com>
2015-01-20 15:29:41 -0500

    Consolidate classes into calculator class
    * made "helper classes" private methods
    * removed tests for "helper classes"

--- lib/arpad.rb
+++ lib/arpad.rb
@@ -4,31 +4,27 @@ module Arpad
   class Calculator
     def self.calculate_elo(elo_one, elo_two, outcome, config = {})
       k_factor = config[:k_factor] || 16
-      transformed_rating_one = TransformedRatingCalculator.
-        calculate_transformed_rating(elo_one)
-      transformed_rating_two = TransformedRatingCalculator.
-        calculate_transformed_rating(elo_two)
-      expected_score = ExpectedScoreCalculator.
-        calculate_expected_score(transformed_rating_one, transformed_rating_two)
-      score_value = ScoreValueCalculator.calculate_score_value(outcome)
+      transformed_rating_one = calculate_transformed_rating(elo_one)
+      transformed_rating_two = calculate_transformed_rating(elo_two)
+      expected_score = calculate_expected_score(
+        transformed_rating_one,
+        transformed_rating_two
+      )
+      score_value = calculate_score_value(outcome)
 
       (elo_one + (k_factor * (score_value - expected_score))).to_i
     end
-  end
 
-  class TransformedRatingCalculator
+    private
+
     def self.calculate_transformed_rating(elo)
       (10**(elo.to_f / 400))
     end
-  end
 
-  class ExpectedScoreCalculator
     def self.calculate_expected_score(t_rating_one, t_rating_two)
       t_rating_one / (t_rating_one + t_rating_two)
     end
-  end
 
-  class ScoreValueCalculator
     def self.calculate_score_value(outcome)
       if outcome == "win"
         1
